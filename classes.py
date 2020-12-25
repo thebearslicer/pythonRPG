5ac631211541717b5c4281860fd3be4c21afa460
thebearslicer <70990720+thebearslicer@users.noreply.github.com>
2020-12-24 23:16:20 -0500

    Add files via upload

--- classes.py
+++ classes.py
@@ -1,5 +1,5 @@
 class monster:
-    def __init__(self, name, lvl, maxHp, attack, mana)
+    def __init__(self, name, lvl, maxHp, attack, mana):
         self.name = name
         self.lvl = lvl
         self.maxHp = maxHp
@@ -7,21 +7,32 @@ class monster:
         self.mana = mana
         
 class location:
-    def __init__(self, name, whatIs, shopLvl)
+    def __init__(self, name, whatIs, shopLvl):
         self.name = name
         self.whatIs = whatIs
         self.shopLvl = shopLvl
         
 class spell:
-    def __init__(self, name, dmg, cost, healValue)
+    def __init__(self, name, dmg, cost, healValue):
         self.name = name
         self.dmg = dmg
         self.cost = cost
         self.healValue = healValue
 
 class consumable:
-    def __init__(self, name, heal, mpheal)
+    def __init__(self, name, heal, mpheal):
         self.name = name
         self.heal = heal
         self.mpheal = mpheal
         
+class weapons:
+    def __init__(self, name, baseDmg, value):
+        self.name = name
+        self.baseDmg = baseDmg
+        self.value = value
+
+class armor:
+    def __init__(self, name, dmgReduc, value):
+        self.name = name            
+        self.dmgReduc = dmgReduc
+        self.value = value
--- main.py
+++ main.py
@@ -10,36 +10,55 @@ print("The dark lord must be stopped, go now hero")
 
 def shop(shopLvl):
     print("What would you like to buy?\n")
-    if shopLvl = 1:                         #idk how to make this better
+    if shopLvl == 1:                         #idk how to make this better
         shopItems = {
             "Longsword" : "20gp",
             "Leather armor" : "30gp",
             "Potion" : "5gp",
             "Elixer" : "5gp"
         }
-    if shopLvl = 2:         #add up to 5 shopLvl, then cry
+    if shopLvl == 2:         #add up to 5 shopLvl, then cry
         shopItems = {
             "Shiny sword" : "35gp",
             "Chainmail" : "50gp",
             "Potion" : "5gp",
             "Elixer" : "5gp"
         }
-    if shopLvl = 3:
+    if shopLvl == 3:
         shopItems = {
             "Platinum Sword" : "50gp",
             "Plate armor" : "75gp",
             "Ultra Potion" : "15gp",
             "Ultra Elixer" : "15gp"
         }
+    if shopLvl == 4:
+        shopItems = {
+            "Blade of Heros" : "100gp",
+            "Diamond Armor" : "150gp",
+            "Ultra Potion" : "15gp",
+            "Ultra Elixer" : "15gp"
+        }
+    if shopLvl == 5:
+        shopItems = {
+            "Legendary Blade" : "250gp",
+            "Legendary Armor" : "350gp",
+            "Super Potion" : "30gp",
+            "Super Elixer" : "30gp"
+        }
+        
+    print(shopLvl)
 
-BlackHollows = location("Black Hollows", "Heros starting town", 1)
+BlackHollows = location("Black Hollows", "Heros starting town", 1) #location is not defined
 
-    option1 = "1. Go to shop\n"
-    option2 = "2. Battle in field\n"
-    option3 = "3. Countinue towards the Evil Lords castle\n" #reseting options is dumb
-    option4 = "4. What is this place?" #whatever
-    option5 = "5.Equip"
-    print(option1, option2, option3, option4, option5)
+option1 = "1. Go to shop\n"
+option2 = "2. Battle in field\n"
+option3 = "3. Countinue towards the Evil Lords castle\n" #reseting options is dumb
+option4 = "4. What is this place?" #whatever
+option5 = "5.Equip"
+print(option1, option2, option3, option4, option5)
     
-    playerChoice = int(input("1/2/3/45"))
+playerChoice = int(input("1/2/3/4/5"))
+    
+if playerChoice == 1:
+    shop(1)
     
